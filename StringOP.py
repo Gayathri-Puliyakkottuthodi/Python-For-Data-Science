e09898d94ab0f7bcd002c39751f4107881129479
GayathriRamakrishnan <gayathri4sngc@gmail.com>
2020-05-17 18:30:58 +0530

    Add files via upload

---
+++ StringOP.py
@@ -0,0 +1,18 @@
+x="Harry Potter"
+print(x[0])#print first element of string
+print(x[-1])#print last element of string
+
+print(x[0:2])#print elements in between indexes 0 and 2
+a=x[3:7]
+print(a)
+
+print(x[::2])#print characters with a gap of 2
+print(x[0:5:2])#print char with gap 2 in b/w 0 to 5
+
+#concatenate strings
+y="is the best"
+print(x+y)
+z=x+y+"book ever"
+print(z)
+#multiply string
+print(3*x)
